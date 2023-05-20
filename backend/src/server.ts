9413010c3340326274eebdfbeb8f6796fba7225d
Jonathas Lopes <jon.athasmoreira@hotmail.com>
2023-05-20 00:28:18 -0300

    WIP third class

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,22 @@
+import 'dotenv/config'
+
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true, // todas URLs de frontend poderão acessar nosso backend ou, voce pode colocar apenas as rotas que irão acessar substintuindo o 'true' por [<url>]
 })
+
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
